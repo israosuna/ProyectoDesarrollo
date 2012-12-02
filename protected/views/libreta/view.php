477384c6343e166ed63d7dfeb8bc3a4e2a2ca0b5
GraLucena <Graciex.ls@gmail.com>
2012-12-02 15:33:47 -0430

    Libretas

--- protected/views/libreta/view.php
+++ protected/views/libreta/view.php
@@ -5,21 +5,21 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Libreta', 'url'=>array('index')),
-	array('label'=>'Create Libreta', 'url'=>array('create')),
-	array('label'=>'Update Libreta', 'url'=>array('update', 'id'=>$model->id_libreta)),
-	array('label'=>'Delete Libreta', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id_libreta),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Listar Libretas', 'url'=>array('index')),
+	array('label'=>'Crear Libreta', 'url'=>array('create')),
+	array('label'=>'Modificar Libreta', 'url'=>array('update', 'id'=>$model->id_libreta)),
+	array('label'=>'Borrar Libreta', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id_libreta),'confirm'=>'Are you sure you want to delete this item?')),
 	//array('label'=>'Manage Libreta', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Libreta</h1>
+<h1>Libreta</h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id_libreta',
-		'id_usuario',
+		//'id_libreta',
+		//'id_usuario',
 		'nombre',
 		'fecha',
 	),
