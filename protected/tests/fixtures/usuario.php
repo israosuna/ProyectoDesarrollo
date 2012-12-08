3056620a2db2cc2b6a653ee91e2c391fcbea79e9
IsraOsuna <osuna.isra@gmail.com>
2012-12-08 18:08:54 -0430

    Otra vez lo q sirve

---
+++ protected/tests/fixtures/usuario.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	
+	'pruebaConexion'=>array(
+                'id_usuario'=>'1',
+                'usuario' => 'israosuna',
+                'clave' => '1234',
+                'token' => 'a:2:{s:5:"token";s:15:"f5l4mg2l3pkyzq1";s:12:"token_secret";s:15:"v4ude01xhohvgb7";}',
+
+              
+            ),      
+        );
