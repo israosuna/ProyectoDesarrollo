5585ff2f379c5ff7d6f73dafaf062f67d4231732
IsraOsuna <osuna.isra@gmail.com>
2012-12-08 18:19:05 -0430

    tratando de hacer la unitaria de busqueda!

---
+++ protected/tests/unit/busquedaTest.php
@@ -0,0 +1,15 @@
+<?php
+class BusquedaTest extends CTestCase
+{
+        public $fixtures = array(
+        'conexiones' => 'Usuario',
+    );
+
+    /*
+     * Test if we have database driver active
+     */
+    public function testBusqueda()
+    {
+        
+    }
+}
